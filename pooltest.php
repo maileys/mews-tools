b0a21e95edf4c5d5ec7b08dbbf6d3e8669e5b51b
maileys <git@scm.id.au>
2015-01-29 23:31:22 +0800

    Fixed pooltest.php

--- pooltest.php
+++ pooltest.php
@@ -3,49 +3,121 @@
 // Saxon Mailey 2015
 // saxon at scm dot id dot au
 
-//session_name("themews");
 session_start();
 
 require 'db_mewsmisc.php';
 
-if (isset($_GET['geolocation'])) {
-	$latitude  = $_GET['latitude'];
-	$longitude = $_GET['longitude'];
-	$_SESSION['latitude']  = $latitude;
-	$_SESSION['longitude'] = $longitude;
-	header("latitude: ${latitude}");
-	header("longitude: ${longitude}");
-	$_SESSION['geolocation']  = 'set';
-
-	# DO DB UPDATE
-	$IP=$_SERVER['REMOTE_ADDR'];
-	$connection = mysql_connect($DBHOST,$DBUSER,$DBPASS);
-	mysql_select_db($DBNAME,$connection);
-	$SQL = 'INSERT INTO location (ip,latitude,longitude) values (' . "'$IP',$latitude,$longitude)";
-	$result = mysql_query($SQL,$connection) or die('cannot execute SQL');
-
-} else {
-
 ?>
-<!DOCTYPE html>
-
-
+<!DOCTYPE HTML>
 <html>
 <body>
 <?php
 
-	if (isset($_SESSION['geolocation'])) {
-		echo 'Cached Location Data:' . "<br>\n";
-		echo 'Latitude: '  . $_SESSION['latitude']  . "<br>\n";
-		echo 'Longitude: ' . $_SESSION['longitude'] . "<br>\n";
-		echo '<A HREF="http://google.com/maps/place/' . $_SESSION['latitude'] . ',' . $_SESSION['longitude'] . '">Google Maps</A>' . "\n";
-	} else {
+$validation = 1;
 
+if(isset($_POST['chlorine']))   { $chlorine   = $_POST['chlorine']; }    else { $chlorine = ''; $validation = 0; }
+if(isset($_POST['ph']))         { $ph         = $_POST['ph']; }          else { $ph = ''; $validation = 0; }
+if(isset($_POST['stabiliser'])) { $stabiliser = $_POST['stabiliser']; }  else { $stabiliser = ''; $validation = 0; }
+if(isset($_POST['alkalinity'])) { $alkalinity = $_POST['alkalinity']; }  else { $alkalinity = ''; $validation = 0; }
+if(isset($_POST['salt']))       { $salt       = $_POST['salt']; }        else { $salt = 0; }
 
+if(isset($_POST['latitude']))   { $latitude   = $_POST['latitude']; }    else { $latitude = 0; }
+if(isset($_POST['longitude']))  { $longitude  = $_POST['longitude']; }   else { $longitude = 0; }
+if($latitude == '') { $latitude = 0; }
+if($longitude == '') { $longitude = 0; }
+
+if ($validation == 1) {
+	# DO DB UPDATE
+	$IP=$_SERVER['REMOTE_ADDR'];
+	$connection = mysql_connect($DBHOST,$DBUSER,$DBPASS);
+	mysql_select_db($DBNAME,$connection);
+	$SQL = 'INSERT INTO pooltest (ip,latitude,longitude,chlorine,ph,stabiliser,alkalinity,salt) values (' . "'$IP',$latitude,$longitude,$chlorine,$ph,$stabiliser,$alkalinity,$salt)";
+	echo '<!-- SQL: ' . $SQL . '-->' . "\n";
+	$result = mysql_query($SQL,$connection) or die('cannot execute SQL');
+	if ($result == 1) {
+		echo "Record Updated\n<p>\n";
+		echo "<table>\n";
+		echo "<tr><td>Chlorine:</td><td>$chlorine</td></tr>\n";
+		echo "<tr><td>pH:</td><td>$ph</td></tr>\n";
+		echo "<tr><td>Stabiliser:</td><td>$stabiliser</td></tr>\n";
+		echo "<tr><td>Alkalinity:</td><td>$alkalinity</td></tr>\n";
+		echo "<tr><td>Salt:</td><td>$salt</td></tr>\n";
+		echo "</table>\n";
+		
+	};
+
+} else {
+	echo '<!-- ' . "\n";
+	echo "Chlorine: $chlorine\n";
+	echo "pH: $ph\n";
+	echo "Stabiliser: $stabiliser\n";
+	echo "Alkalinity: $alkalinity\n";
+	echo "Salt: $salt\n";
+	echo '-->' . "\n";
 ?>
-<p id="demo"></p>
-<script>
-var x = document.getElementById("demo");
+<form method=post>
+<table>
+	<tr>
+		<td>Chlorine</td>
+		<td><input type=text name=chlorine list=chlorine>
+			<datalist id=chlorine>
+<?php
+for ($x = 0.8; $x <= 3.0; $x=$x+0.2) { echo "\t\t\t\t<option>" . number_format($x,1) . "</option>\n"; } 
+?>
+			</datalist>
+		</td>
+	</tr>
+	<tr>
+		<td>pH</td>
+		<td><input type=text name=ph list=ph>
+			<datalist id=ph>
+<?php
+for ($x = 6.2; $x <= 8.6; $x=$x+0.2) { echo "\t\t\t\t<option>" . number_format($x,1) . "</option>\n"; }
+?>
+			</datalist>
+		</td>
+	</tr>
+	<tr>
+		<td>Stabiliser</td>
+		<td><input type=text name=stabiliser list=stabiliser>
+			<datalist id=stabiliser>
+<?php
+for ($x = 10; $x <= 100; $x=$x+10) { echo "\t\t\t\t<option>$x</option>\n"; }
+?>
+			</datalist>
+		</td>
+	</tr>
+	<tr>
+		<td>Total Alkalinity</td>
+		<td><input type=text name=alkalinity list=alkalinity>
+			<datalist id=alkalinity>
+<?php
+for ($x = 80; $x <= 200; $x=$x+20) { echo "\t\t\t\t<option>$x</option>\n"; } 
+?>
+			</datalist>
+		</td>
+	</tr>
+	<tr>
+		<td>Salt</td>
+		<td><input type=text name=salt list=salt>
+			<datalist id=salt>
+<?php
+for ($x = 2000; $x <= 8000; $x=$x+100) { echo "\t\t\t\t<option>$x</option>\n"; } 
+?>
+			</datalist>
+		</td>
+	</tr>
+
+</table>
+<input type="submit" value="Submit">
+<P>
+<input type=hidden name=latitude id=latitude><br>
+<input type=hidden name=longitude id=longitude>
+</form>
+<p id="mytext"></p>
+
+<script type="text/javascript">
+var mytext = document.getElementById('mytext');
 
 window.onload = getLocation()
 
@@ -53,77 +125,44 @@ function getLocation() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(showPosition, showError);
 	} else { 
-		x.innerHTML = "Geolocation is not supported by this browser.";
+		mytext.innerHTML = "Geolocation is not supported by this browser.";
 	}
 }
 
 function showPosition(position) {
-	x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;	
-	var latitude=encodeURIComponent(position.coords.latitude)
-	var longitude=encodeURIComponent(position.coords.longitude)
-	mygetrequest.open("GET", "?geolocation=true&latitude="+latitude+"&longitude="+longitude, true)
-	mygetrequest.send(null)
-	x.innerHTML = "Detected Location Data:<br>\n" 
-		+ 'Latitude:'  + position.coords.latitude + "<br>\n"
-		+ 'Longitude:' + position.coords.latitude + "<br>\n"
-		+ '<A HREF="http://google.com/maps/place/' + position.coords.latitude + ',' +position.coords.longitude + "\">Google Maps</A>"; 
+	//mytext.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;	
+	//var latitude=encodeURIComponent(position.coords.latitude);
+	//var longitude=encodeURIComponent(position.coords.longitude);
+	//var mylat=document.getElementById('latitude');
+	//var mylong=document.getElementById('longitude');
+	//mylat.value=encodeURIComponent(position.coords.latitude);
+	//mylong.value=encodeURIComponent(position.coords.latitude);
+	document.getElementById('latitude').value  = encodeURIComponent(position.coords.latitude);
+	document.getElementById('longitude').value = encodeURIComponent(position.coords.longitude);
 }
 
 function showError(error) {
 	switch(error.code) {
 		case error.PERMISSION_DENIED:
-			x.innerHTML = "User denied the request for Geolocation."
+			mytext.innerHTML = "User denied the request for Geolocation."
 			break;
 		case error.POSITION_UNAVAILABLE:
-			x.innerHTML = "Location information is unavailable."
+			mytext.innerHTML = "Location information is unavailable."
 			break;
 		case error.TIMEOUT:
-			x.innerHTML = "The request to get user location timed out."
+			mytext.innerHTML = "The request to get user location timed out."
 			break;
 		case error.UNKNOWN_ERROR:
-			x.innerHTML = "An unknown error occurred."
+			mytext.innerHTML = "An unknown error occurred."
 			break;
 	}
 }
-
-function ajaxRequest(){
-	var activexmodes=["Msxml2.XMLHTTP", "Microsoft.XMLHTTP"] //activeX versions to check for in IE
-	if (window.ActiveXObject){ //Test for support for ActiveXObject in IE first (as XMLHttpRequest in IE7 is broken)
-		for (var i=0; i<activexmodes.length; i++){
-			try{
-				return new ActiveXObject(activexmodes[i])
-			}
-			catch(e){
-			//suppress error
-			}
-		}
-	} else {
-		if (window.XMLHttpRequest) { // if Mozilla, Safari etc
-			return new XMLHttpRequest()
-		} else {
-			return false
-		}
-	}
-}
-
-var mygetrequest=new ajaxRequest()
-mygetrequest.onreadystatechange=function(){
-	if (mygetrequest.readyState==4){
-		if (mygetrequest.status==200 || window.location.href.indexOf("http")==-1){
-			document.getElementById("result").innerHTML=mygetrequest.responseText
-		} else {
-			alert("An error has occured making the request")
-		}
-	}
-}
-
 
 </script>
 
+<?php
+}
+?>
 </body>
 </html>
-
-<?php
-	}
-}
 
