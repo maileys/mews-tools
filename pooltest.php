a0bac6ac2ed267c0a3dffdddd9c33b9d0f9cc9ba
maileys <git@scm.id.au>
2015-01-30 10:41:43 +0800

    added mobile friendly meta tag

--- pooltest.php
+++ pooltest.php
@@ -10,7 +10,11 @@ require 'db_mewsmisc.php';
 ?>
 <!DOCTYPE HTML>
 <html>
+<head>
+	<meta name="HandheldFriendly" content="true" />
+</head>
 <body>
+<H3>The Mews - Pool Testing Log</H3>
 <?php
 
 $validation = 1;
@@ -37,11 +41,11 @@ if ($validation == 1) {
 	if ($result == 1) {
 		echo "Record Updated\n<p>\n";
 		echo "<table>\n";
-		echo "<tr><td>Chlorine:</td><td>$chlorine</td></tr>\n";
-		echo "<tr><td>pH:</td><td>$ph</td></tr>\n";
-		echo "<tr><td>Stabiliser:</td><td>$stabiliser</td></tr>\n";
-		echo "<tr><td>Alkalinity:</td><td>$alkalinity</td></tr>\n";
-		echo "<tr><td>Salt:</td><td>$salt</td></tr>\n";
+		echo "<tr><td> Chlorine: </td><td> $chlorine </td></tr>\n";
+		echo "<tr><td> pH: </td><td> $ph </td></tr>\n";
+		echo "<tr><td> Stabiliser: </td><td> $stabiliser </td></tr>\n";
+		echo "<tr><td> Alkalinity: </td><td> $alkalinity </td></tr>\n";
+		echo "<tr><td> Salt: </td><td> $salt </td></tr>\n";
 		echo "</table>\n";
 		
 	};
@@ -58,7 +62,7 @@ if ($validation == 1) {
 <form method=post>
 <table>
 	<tr>
-		<td>Chlorine</td>
+		<td> Chlorine </td>
 		<td><input type=text name=chlorine list=chlorine>
 			<datalist id=chlorine>
 <?php
@@ -68,7 +72,7 @@ for ($x = 0.8; $x <= 3.0; $x=$x+0.2) { echo "\t\t\t\t<option>" . number_format($
 		</td>
 	</tr>
 	<tr>
-		<td>pH</td>
+		<td> pH </td>
 		<td><input type=text name=ph list=ph>
 			<datalist id=ph>
 <?php
@@ -78,7 +82,7 @@ for ($x = 6.2; $x <= 8.6; $x=$x+0.2) { echo "\t\t\t\t<option>" . number_format($
 		</td>
 	</tr>
 	<tr>
-		<td>Stabiliser</td>
+		<td> Stabiliser </td>
 		<td><input type=text name=stabiliser list=stabiliser>
 			<datalist id=stabiliser>
 <?php
@@ -88,7 +92,7 @@ for ($x = 10; $x <= 100; $x=$x+10) { echo "\t\t\t\t<option>$x</option>\n"; }
 		</td>
 	</tr>
 	<tr>
-		<td>Total Alkalinity</td>
+		<td> Total Alkalinity </td>
 		<td><input type=text name=alkalinity list=alkalinity>
 			<datalist id=alkalinity>
 <?php
@@ -98,7 +102,7 @@ for ($x = 80; $x <= 200; $x=$x+20) { echo "\t\t\t\t<option>$x</option>\n"; }
 		</td>
 	</tr>
 	<tr>
-		<td>Salt</td>
+		<td> Salt </td>
 		<td><input type=text name=salt list=salt>
 			<datalist id=salt>
 <?php
@@ -109,6 +113,7 @@ for ($x = 2000; $x <= 8000; $x=$x+100) { echo "\t\t\t\t<option>$x</option>\n"; }
 	</tr>
 
 </table>
+<p>
 <input type="submit" value="Submit">
 <P>
 <input type=hidden name=latitude id=latitude><br>
